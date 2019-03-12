536cbe78217795fe8ee4c5ae044dd2f7c0652324
azam alias <azamalias@gmail.com>
2019-03-12 16:16:54 +0800

    Use azure-function-express, with correct dependencies file

--- ExpressFunctionApp/index.js
+++ ExpressFunctionApp/index.js
@@ -1,4 +1,4 @@
-const AzureFunctionHandler = require("azure-aws-serverless-express");
+const AzureFunctionHandler = require("azure-function-express").createHandler;
 const app = require("./app");
 
 // Binds the express app to an Azure Function handler
