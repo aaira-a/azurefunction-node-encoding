78a31b9e1db209c8283c62aadd6ee2d2f0c7114b
azam alias <azamalias@gmail.com>
2019-03-12 15:37:44 +0800

    Add handler following previous repo instead of readme

--- ExpressFunctionApp/index.js
+++ ExpressFunctionApp/index.js
@@ -1,5 +1,5 @@
-const createHandler = require("azure-function-express").createHandler;
+const createAzureFunctionHandler = require("azure-function-express").createAzureFunctionHandler;
 const app = require("./app");
 
 // Binds the express app to an Azure Function handler
-module.exports = createHandler(app);
+module.exports = createAzureFunctionHandler(app);
